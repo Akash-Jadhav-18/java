518e6013fe4e57a3f9f0fe66555c2dd8cef9c5d5
Akash-Jadhav-18 <73697628+Akash-Jadhav-18@users.noreply.github.com>
2021-06-28 01:10:46 +0530

    Update Divisible.java

--- Divisible.java
+++ Divisible.java
@@ -5,17 +5,6 @@
 // Input :15
 // Output : Yes
 
-////////////////////////////////////////////////////////////////////////////
-//
-//  Function name : CheckDivisble
-//  Input : Integer
-//  Output : Boolean (True / False)
-//  Description : It is used to check whetheer number is divisible by 3 and 5
-//  Date : 01/03/2021
-//  Author : Piyush Manohar Khairnar
-//
-////////////////////////////////////////////////////////////////////////////
-
 
 import java.util.Scanner;
 class NumberX
